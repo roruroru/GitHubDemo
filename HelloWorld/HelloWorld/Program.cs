1a8b98085acb3e661455d117d6ee6b1ec8f25a9d
Rusi Rusev <roussy.roussev@gmail.com>
2017-06-06 16:55:17 +0300

    Added a line.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, C#!");
+            Console.WriteLine("Bye, C#!");
         }
     }
 }
